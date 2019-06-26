2b91ee5e21f8cb054f30f50985cee54c154cb3e9
Gie-Art <sugiarto@gie-art.com>
2019-06-26 21:39:51 +0700

    - applied bootstrap form style in product - added validations on product create/update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
-	validates :sku, presence: true
+	validates :sku, presence: true, uniqueness: true
 	validates_format_of :sku, :with => /\A[a-zA-Z\d ]*\z/i, :message => "can only contain letters and numbers."
 
-	validates :name, presence: true
+	validates :name, presence: true, uniqueness: true
 	validates :price, presence: true
 	validates :status, presence: true
 end

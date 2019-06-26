63648a7e042a4fe93a91d98daf222ae9ad1f2e7e
Gie-Art <sugiarto@gie-art.com>
2019-06-26 21:44:44 +0700

    update migration

--- db/migrate/20190625144839_create_products.rb
+++ db/migrate/20190625144839_create_products.rb
@@ -1,14 +1,18 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
-  def change
-    create_table :products do |t|
-      t.string :sku
-      t.string :name
-      t.string :slug
-      t.decimal :price, precision: 10, scale: 2
-      t.text :description
-      t.integer :status
+  	def change
+		create_table :products do |t|
+			t.string :sku
+			t.string :name
+			t.string :slug
+			t.decimal :price, precision: 10, scale: 2
+			t.text :description
+			t.integer :status
 
-      t.timestamps
-    end
-  end
+			t.timestamps
+
+			add_index :products, :sku, unique: true
+			add_index :products, :name, unique: true
+			add_index :products, :slug, unique: true
+		end
+  	end
 end

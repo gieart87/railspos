74ed7f69b7379892ffb964e8c4121c0818f57db1
Gie-Art <sugiarto@gie-art.com>
2019-06-29 00:50:18 +0700

    update profile for logged in user

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+
+	protected
+
+	  	def update_resource(resource, params)
+	    	# Require current password if user is trying to change password.
+	    	return super if params["password"]&.present?
+
+	    	# Allows user to update registration information without password.
+	    	resource.update_without_password(params.except("current_password"))
+	  	end
+end

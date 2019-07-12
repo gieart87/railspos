8c1336b2f7f471fb38ca272862e8481ac7cccbde
sugiarto@gie-art.com <sugiarto@gie-art.com>
2019-07-12 16:07:53 +0700

    update generate_code method

--- app/models/order.rb
+++ app/models/order.rb
@@ -32,11 +32,27 @@ class Order < ApplicationRecord
 	end
 
 	private
+		# def generate_code
+		# 	chars = (0..9).to_a
+		# 	loop do
+		# 		order_code = (0...8).collect { chars[Kernel.rand(chars.length)] }.join
+		# 		break order_code unless Order.where(order_code: order_code).exists?
+		# 	end
+		# end
+
 		def generate_code
-			chars = (0..9).to_a
-			loop do
-				order_code = (0...8).collect { chars[Kernel.rand(chars.length)] }.join
-				break order_code unless Order.where(order_code: order_code).exists?
+			date_code = DateTime.now.strftime('%Y%m%d')
+
+			sql_last_code = "SELECT MAX(order_code) AS last_code FROM orders WHERE order_code LIKE '#{date_code}%' LIMIT 1"
+			last_order_code = ActiveRecord::Base.connection.execute(sql_last_code).first[0]
+
+			order_code = date_code + '0001'
+			if !last_order_code.nil?
+				last_order_number = last_order_code[8,4]
+				next_order_number = '%04d' % (last_order_number.to_i + 1)
+				order_code = date_code + next_order_number
 			end
+			
+			return order_code
 		end
 end

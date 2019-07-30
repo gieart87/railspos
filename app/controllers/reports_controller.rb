d477ede7c4a23ab0fb95dfeb5ef28bfac7685f5c
Gie-Art <sugiarto@gie-art.com>
2019-07-30 21:08:37 +0700

    update sales report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,16 @@ class ReportsController < ApplicationController
 
 	def sales
 		@current_sub_menu = 'sales'
-		@items = Item.select('orders.order_date','products.sku','products.name','items.qty').joins(:order).joins(:product).where('orders.status = 2').order('orders.order_date DESC').paginate(page: params[:page], per_page: 10)
+		@items = Item.select(
+			'DATE(orders.order_date) AS sales_date',
+			'products.sku',
+			'products.name',
+			'SUM(items.qty) qty')
+		.joins(:order).joins(:product)
+		.where('orders.status = 2')
+		.group('sales_date','products.id')
+		.order('sales_date DESC')
+		.paginate(page: params[:page], per_page: 10)
 	end
 
 	def revenue

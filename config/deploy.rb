175ca2bcb608946b1235cffb22218ca1342b0b28
Gie-Art <sugiarto@gie-art.com>
2019-08-03 09:30:37 +0700

    update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,7 +75,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 append :linked_files, 'config/database.yml'
-append :linked_files, %w{config/master.key}
+append :linked_files, 'config/master.key'
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'

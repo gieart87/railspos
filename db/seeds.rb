33f37759b38b5523c6c4f36dcdf8f0b360445051
sugiarto <sugiarto@gie-art.com>
2020-07-05 01:53:12 +0700

    updated the db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
   role_user = Role.create(:name => 'user')  
   role_admin = Role.create(:name => 'admin')  
-  User.create(:email => 'user@example.com' , :password => 'user123', :role_id => role_user.id)   
-  User.create(:email => 'admin@example.com' , :password => 'admin123', :role_id => role_admin.id)  
+  User.create(:email => 'user@example.com' ,  :password => 'user123', :role_id => role_user.id, :first_name => 'user', :last_name => '-')   
+  User.create(:email => 'admin@example.com' , :password => 'admin123', :role_id => role_admin.id, :first_name => 'admin', :last_name => '-')  
